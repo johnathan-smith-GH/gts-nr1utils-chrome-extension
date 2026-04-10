6cbd1de3311b761844704f06fedddd95e308dfe4
Johnathan Smith <johnathansmith@newrelic.com>
2026-04-10 12:10:07 -0700

    Add dashboard scroll notice to NRQL requests pane
    When a dashboard is detected, show a notice that some widget queries
    may only fire when scrolled into view, prompting users to scroll
    through all widgets to capture all NRQL requests.

--- dist/components/RequestsPage.js
+++ dist/components/RequestsPage.js
@@ -253,7 +253,10 @@ const RequestsPage = props => {
     onToggleSelect: toggleSelectedIndex,
     onSelectAllVisible: selectAllVisible,
     onClearSelected: clearSelectedIndices
-  }), (function () {
+  }), widgetMap && widgetMap.length > 0 && /*#__PURE__*/React.createElement("div", {
+    className: "App-dashboardNotice"
+  }, "Dashboard detected \u2014 some widget queries may only fire when scrolled into view. Scroll through all widgets on the page to capture all NRQL requests."),
+  (function () {
     // Show inaccessible widgets banner when widgets exist that the user can't access
     if (!widgetMap || !widgetMap.length) return null;
     var inaccessible = widgetMap.filter(function (w) { return w.inaccessible; });
